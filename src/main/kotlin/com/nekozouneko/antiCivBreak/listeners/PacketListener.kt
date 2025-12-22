1266f712753381b6b15dad8d2b2f9364a1ad8076
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-12-23 00:04:33 +0900

    fix: PacketCheckerがエンドストーン以外でも動作してしまうバグを修正

--- src/main/kotlin/com/nekozouneko/antiCivBreak/listeners/PacketListener.kt
+++ src/main/kotlin/com/nekozouneko/antiCivBreak/listeners/PacketListener.kt
@@ -6,6 +6,7 @@ import com.github.retrooper.packetevents.protocol.packettype.PacketType
 import com.github.retrooper.packetevents.protocol.player.DiggingAction
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerDigging
 import com.nekozouneko.antiCivBreak.AntiCivBreak
+import org.bukkit.Material
 
 class PacketListener : PacketListener {
     override fun onPacketReceive(event: PacketReceiveEvent) {
@@ -14,7 +15,15 @@ class PacketListener : PacketListener {
         val action = WrapperPlayClientPlayerDigging(event)
         val manager = AntiCivBreak.getManager(event.user.uuid) ?: return
 
-        if (action.action == DiggingAction.FINISHED_DIGGING) {
+        //Material Check
+        if(action.action == DiggingAction.START_DIGGING || action.action == DiggingAction.FINISHED_DIGGING){
+            val blockPos = action.blockPosition
+            val world = manager.getPlayer().world
+            val material = world.getBlockAt(blockPos.x, blockPos.y, blockPos.z).type
+            if(material != Material.END_STONE) return
+        }
+
+        if(action.action == DiggingAction.FINISHED_DIGGING) {
             for(handler in AntiCivBreak.packetHandlers) handler.handle(manager, action, event)
         }
 
