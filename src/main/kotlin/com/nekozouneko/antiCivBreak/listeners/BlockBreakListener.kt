4a0fccdc1c88622bcd93417921bfc75eff9c12a3
るんく君 <91005690+RunqRun@users.noreply.github.com>
2025-12-23 18:47:33 +0900

    fix: リスナー優先度を設定（プラグイン側のキャンセルを無視しないように）

--- src/main/kotlin/com/nekozouneko/antiCivBreak/listeners/BlockBreakListener.kt
+++ src/main/kotlin/com/nekozouneko/antiCivBreak/listeners/BlockBreakListener.kt
@@ -4,11 +4,12 @@ import com.nekozouneko.antiCivBreak.AntiCivBreak
 import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 
 class BlockBreakListener : Listener {
-    @EventHandler
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     fun onBreak(e: BlockBreakEvent){
         if(e.player.gameMode != GameMode.SURVIVAL) return
         if(e.block.type != Material.END_STONE) return
